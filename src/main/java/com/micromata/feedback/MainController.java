fec2f0588dbf9917ff7c7cf454441dcf1a271357
Michael Lesniak <mlesniak@micromata.de>
2015-04-25 15:30:44 +0200

    Feedback submit-action.

--- src/main/java/com/micromata/feedback/MainController.java
+++ src/main/java/com/micromata/feedback/MainController.java
@@ -1,7 +1,12 @@
 package com.micromata.feedback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles request to /.
@@ -10,8 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class MainController {
+  private static final Logger LOG = LoggerFactory.getLogger(MainController.class);
+
   @RequestMapping("/")
   public String index() {
+    LOG.info("Feedback page called");
     return "index";
+  }
+
+  @RequestMapping(value = "/submit", method = RequestMethod.POST)
+  public ModelAndView submit(@RequestParam("text") String text) {
+    LOG.info("Feedback text='{}'", text);
+    ModelAndView mav = new ModelAndView("index");
+    mav.addObject("submit", true);
+    return mav;
   }
 }
