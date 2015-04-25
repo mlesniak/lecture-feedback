6ba71038505eaba7a78f0b6c400f98d7b3e90449
Michael Lesniak <mlesniak@micromata.de>
2015-04-25 17:17:03 +0200

    Logging configuration.

--- src/main/java/com/micromata/feedback/Main.java
+++ src/main/java/com/micromata/feedback/Main.java
@@ -1,7 +1,11 @@
 package com.micromata.feedback;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Main application class.
@@ -10,7 +14,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Main {
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
+  }
+
+  @PostConstruct
+  public void started() {
+    LOG.info("Application started");
   }
 }
