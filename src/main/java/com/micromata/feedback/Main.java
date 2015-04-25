a9c9865ca3fb27c88074761aee909e05e2836632
Michael Lesniak <mlesniak@micromata.de>
2015-04-25 14:33:07 +0200

    Initial commit.

---
+++ src/main/java/com/micromata/feedback/Main.java
@@ -0,0 +1,12 @@
+package com.micromata.feedback;
+
+/**
+ * Main application class.
+ *
+ * @author Michael Lesniak (mlesniak@micromata.de)
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
